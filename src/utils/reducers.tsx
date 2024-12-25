dec80831efd501851d51f8575e04d3d191741724
mac <mac@MacBook-Pro-de-mac.local>
2024-12-25 15:20:38 +0100

    add redux saga config

---
+++ src/utils/reducers.tsx
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import exampleReducer from './exampleReducer'; // Example reducer
+
+const rootReducer = combineReducers({
+//   example: exampleReducer,
+});
+
+export default rootReducer;
